6ffc1a3245d2c509e45a3f11afdc841825b180be
ezhukovskaya <tthebestpersonofaip@gmail.com>
2019-12-13 13:02:33 +0300

    2-3 mini-task

--- src/test/java/pageObjects/responses/responsePosts.java
+++ src/test/java/pageObjects/responses/responsePosts.java
@@ -2,18 +2,22 @@ package pageObjects.responses;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import framework.utils.APIUtils;
+import org.apache.log4j.Logger;
 import pageObjects.models.Post;
+import tests.TC1;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class responsePosts {
     private static final String POSTS = "posts/";
+    static final Logger log = Logger.getLogger(responsePosts.class);
 
     public static ArrayList<Post> getPosts(String url) {
         url += POSTS;
         ObjectMapper objectMapper = new ObjectMapper();
         ArrayList<Post> posts = new ArrayList<Post>();
+        log.info("Adding JSON objects into the array");
         for (int i = 0; i < APIUtils.getJsonNodeSize(url); i++) {
             try {
                 posts.add(objectMapper.readValue(APIUtils.getJsonArray(url).getJSONObject(i).toString(), Post.class));
@@ -27,6 +31,7 @@ public class responsePosts {
     public static Post getPost(String url, int id) {
         url += POSTS;
         ObjectMapper objectMapper = new ObjectMapper();
+        log.info("Taking " + id + " JSON object");
         try {
             return objectMapper.readValue(APIUtils.getJsonArray(url).getJSONObject(id).toString(), Post.class);
         }catch(Exception e) {
--- src/test/java/tests/TC1.java
+++ src/test/java/tests/TC1.java
@@ -12,15 +12,21 @@ import java.util.ArrayList;
 public class TC1 {
     static final Logger log = Logger.getLogger(TC1.class);
     private static final String PAGE = PropertiesRead.readFromFrameworkConfig("page");
-    private static final int POST_ID = 99;
+    private static final int POST_ID_99 = 99;
+    private static final int POST_ID_150 = 150;
 
     @Test
     public void javaScriptPageTest() {
         ArrayList<Post> posts = responsePosts.getPosts(PAGE);
         log.info("Check if Post are ascending");
         Assert.assertTrue(responsePosts.isSorted(posts), "Posts is not correct or not sorted");
-        Post testPost = responsePosts.getPost(PAGE, POST_ID);
-        Assert.assertNotNull(testPost.getTitle(), "Title or Body is empty");
+        Post testPost99 = responsePosts.getPost(PAGE, POST_ID_99);
+        String title = testPost99.getTitle();
+        String body = testPost99.getBody();
+        log.info("Check if title and body are not empty");
+        Assert.assertEquals(title.isEmpty(), body.isEmpty(), "Body or title is empty");
+        Post testPost150 = responsePosts.getPost(PAGE, POST_ID_150);
+        log.info("Check if Post " + POST_ID_150 + " is null");
+        Assert.assertNull(testPost150, "Post id=" + POST_ID_150 + " exists");
     }
-
 }
